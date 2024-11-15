17df49868e7b76664cb018f7b5d6daa712eb377f
solennyD <deleonsolenny28@gmail.com>
2024-11-14 23:01:01 -0400

    correcion de boton inicio

--- web/hospitales.php
+++ web/hospitales.php
@@ -58,7 +58,7 @@
             </div>
  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="http://localhost/Medic Care/web/nuevocliente.php" class="nav-btn"><i class='glyphicon glyphicon-list-alt'></i> Inicio <span class="sr-only">(current)</span></a></li>
+                    <li class="active"><a href="./user.php" class="nav-btn"><i class='glyphicon glyphicon-list-alt'></i> Inicio <span class="sr-only">(current)</span></a></li>
                     
               
                     <li><a href="usuario.php" onclick="frmCliente()" class="nav-btn"> <i class='glyphicon glyphicon-user'></i> Usuarios</a></li>
